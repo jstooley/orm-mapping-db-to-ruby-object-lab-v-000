9ab808249fa756a992e3cfca037be7f90841e09d
Justin Tooley <jstooley823@gmail.com>
2017-10-29 18:13:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(X)
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10 LIMIT X
     SQL
