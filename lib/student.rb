7ef0258336654aaaec4ad5f09ae1485fdbb06ec0
Justin Tooley <jstooley823@gmail.com>
2017-10-29 18:07:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
     SELECT * FROM students WHERE grade = 9
     SQL
 
-    DB[:conn].execute(sql,grade).map do |row|
+    DB[:conn].execute(sql, 9 ).map do |row|
       self.new_from_db(row)
     end
   end
