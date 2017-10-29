187b5b91d8a8e2b3b220f822c98cb210494c1fd9
Justin Tooley <jstooley823@gmail.com>
2017-10-29 18:30:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,6 +97,10 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_database(row)
+    end.first
   end
 
 
